9d1ff9d37d90acbd65b891038549b432579eb6b0
nikatnikris <nik@nikris.co.uk>
2020-06-04 15:29:45 +0100

    working version

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# CLX3180ScanPS
-Powershell to control scanner on Samsung CLX-3180
+# Powershell control for Samsung CLX3180 scanner feature
+The Samsung CLX-3180 all-in-one printer/scanner has a built in flat bed scanner feature.  This feature normally requires specific software to control it which requires a GUI.  This project allows the scanner to capture images and place the output files in a share so it can be used on a server via PS remoting.  I.e. the script can be placed on a server or workstation where the printer is attached, and a shortcut can be added to other computers that can trigger a scan an make the image available in a share.
+
+The script is currently focussed around this specific printer and my home server configuration, but could be easily extended to other devices, and to write the output to an alternative network location such as a NAS share.
+
+The script was written for Powershell 5 on Windows and hasn't been tested anywhere else, but should be adaptable for other versions / platforms.
+
+
+
+## Usage
+
+Clone the repo to a local path:
+
+```powershell
+git clone https://github.com/nikatnikris/CLX3180ScanPS.git
+```
+
+
+
+Run the script
+
+```powershell
+./scan.ps1 -CreatePath -ImageType JPEG -Resolution 300 -JPEGQuality 99
+```
+
+Parameter meanings:
+
+`CreatePath` - optional.  If this is included, the image destination folder and SMB share are automatically created if they are missing.  Requires **administrator**
+
+`ImageType` - default JPEG.  Other options are PNG, GIF and TIFF
+
+`Resolution` - default 300.  DPI scanning quality.  Options are 75, 150, 300, 600, 1200
+
+`JPEGQuality` - default 90.  JPEG image compression quality.  Value can be between 1 and 100
+
+
+
+## Adapting for your scanner / server
+
+These lines can be updated in the script:
+
+`$filePath = "C:\Shared\Scanner"` - you can set this to whatever works for you.  UNC paths should be OK too
+
+`$deviceID = "{6BDD1FC6-810F-11D0-BEC7-08002BE2092F}\0000"` - you can update this to point to any WIA (Windows Image Acquisition) devices connected to your computer.  These can be found by:
+
+```powershell
+$deviceManager = New-Object -ComObject WIA.DeviceManager
+$deviceManager.DeviceInfos
+```
+
+
+
--- scan.ps1
+++ scan.ps1
@@ -1,6 +1,9 @@
 ﻿ [CmdletBinding()]  
  param (
-    [switch]$CreatePath = $false
+    [Switch]$CreatePath = $false,
+    [ValidateSet(“PNG", "GIF", "JPEG", "TIFF")][String]$ImageType = "JPEG",
+    [ValidateSet("75", “150", "300", "600", "1200")][String]$Resolution = "300",
+    [ValidateRange(1,100)][String]$JPEGQuality = "90"
  )  
 
 
@@ -11,12 +14,14 @@ $deviceID = "{6BDD1FC6-810F-11D0-BEC7-08002BE2092F}\0000"
 #---------------------------------------------------------------------------------------------------------------
 
 
+# check for admin
 function Test-Administrator {  
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
 
 
+# if requested, create path/share
 if(-not (Test-Path $filePath)){
     if($CreatePath){
         if(-not (Test-Administrator)){
@@ -35,26 +40,51 @@ if(-not (Test-Path $filePath)){
     }
 }
 
+
 # connect to device
-$deviceManager = new-object -ComObject WIA.DeviceManager
+$deviceManager = New-Object -ComObject WIA.DeviceManager
 $device = ($deviceManager.DeviceInfos | where {$_.DeviceID -eq $deviceID}).Connect()    
 
 
-$wiaFormatJPG = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}"
+# set guid from image type param
+$wiaFormatPNG = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatGIF = "{B96B3CB0-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatJPEG = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatTIFF = "{B96B3CB1-0728-11D3-9D7B-0000F81EF32E}"
+if ($ImageType -eq "PNG"){$wiaFormat = $wiaFormatPNG}
+if ($ImageType -eq "GIF"){$wiaFormat = $wiaFormatGIF}
+if ($ImageType -eq "JPEG"){$wiaFormat = $wiaFormatJPEG}
+if ($ImageType -eq "TIFF"){$wiaFormat = $wiaFormatTIFF}
+
+
+if ($Resolution -eq "1200"){ Write-Output "! Scanning at 1200 DPI can take a few minutes !" }
+Write-Output "scanning image, please wait..."
+
+#set properties and scan
 foreach ($item in $device.Items) { 
     foreach($prop in $item.Properties){
-        if(($prop.PropertyID -eq 6147) -or ($prop.PropertyID -eq 6148)){ $prop.Value = 300 }
+        if(($prop.PropertyID -eq 6147) -or ($prop.PropertyID -eq 6148)){ $prop.Value = $Resolution }
     }
-    $image = $item.Transfer($wiaFormatJPG) 
+    $image = $item.Transfer($wiaFormat) 
 }    
 
-if($image.FormatID -ne $wiaFormatPNG){
-    $imageProcess = new-object -ComObject WIA.ImageProcess
-    $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
-    $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = $wiaFormatJPG
-    $imageProcess.Filters.Item(1).Properties.Item("Quality").Value = 90
-    $image = $imageProcess.Apply($image)
-}
 
-$fileName = "C:\Shared\Scanner\$((Get-Date).ToString("yyyy-MM-dd-HHmmss")).jpg"
-$image.SaveFile("$filename")
+#process image
+$imageProcess = New-Object -ComObject WIA.ImageProcess
+$imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
+$imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = $wiaFormat
+$imageProcess.Filters.Item(1).Properties.Item("Quality").Value = $JPEGQuality
+$image = $imageProcess.Apply($image)
+
+
+# figure out filename extension
+if ($ImageType -eq "PNG"){$fileExt = "png"}
+if ($ImageType -eq "GIF"){$fileExt = "gif"}
+if ($ImageType -eq "JPEG"){$fileExt = "jpg"}
+if ($ImageType -eq "TIFF"){$fileExt = "tiff"}
+
+
+#save
+$fileName = "$((Get-Date).ToString("yyyy-MM-dd-HHmmss")).$fileExt"
+$image.SaveFile((Join-Path $filePath $filename))
+Write-Output "'$filename' saved to '$filePath'"
